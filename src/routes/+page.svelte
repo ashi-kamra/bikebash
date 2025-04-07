5381be16d12fecd77c84baa49c54c26c30d44228
Ashi Kamra <ak118@wellesley.edu>
2025-04-07 11:47:21 -0400

    map lab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {onMount} from "svelte";
+    import mapboxgl from "mapbox-gl";
+    import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+
+    mapboxgl.accessToken = "pk.eyJ1IjoiYWthbXJhMTE4IiwiYSI6ImNtOTRrbHg3aTB6MWMyanB5bmh3c2dwMmYifQ.AUIoUyEPsIoAgVXIIIjmdg";
+
+    onMount(() => {
+        let map = new mapboxgl.Map({
+            container: 'map',
+            center:[-71.09415, 42.36027],
+            minZoom: 10,
+            maxZoom: 20,
+            zoom: 12,
+            style: "mapbox://styles/mapbox/light-v11",
+        })
+    })
+
+</script>
+
+<h1>🚲BikeBash🚲</h1>
+<p>Visualizing bike traffic in Boston over time</p>
+
+<div id="map">
+
+</div>
+
+<style>
+    @import url("$lib/global.css");
+
+    #map {
+        flex: 1;
+        background-color: blue
+    }
+</style>
